fc59181bacf7b2ddf75861a5f250a01daab1af87
tanatana <aiueo2003haru@gmail.com>
2025-12-14 01:57:21 +0900

    初期コミット

---
+++ ActionTwo/Source/ActionTwo/ActionTwoGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ActionTwoGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AActionTwoGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AActionTwoGameMode();
+};
+
+
+
